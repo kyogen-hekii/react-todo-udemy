896ad83cb2a327fbe4744d36d34f4d1c26ff4ebe
kyogen-hekii <kyogen.hekii@gmail.com>
2019-12-29 17:00:01 +0900

    understand jsx (& a little hooks)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [value, setValue] = useState("");
+  useEffect(() => {
+    console.log(value);
+  },[value])
+  
+  const greeting = 'Hi';
+  const dom = 
+    <>
+      <div>
+        {greeting} world!
+      </div>
+      <input type="text" onChange = {(e) => setValue(e.target.value)}/>
+    </>;
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    dom
   );
 }
 
